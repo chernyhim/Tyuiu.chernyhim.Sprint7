6380897f4fb71ae72f6b1b252e21d9b3e46b3248
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-12-19 14:35:02 +0500

    Add project files.

---
+++ Tyuiu.chernyhim.Sprint7.Project.V2.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.chernyhim.Sprint7.Project.V2.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.chernyhim.Sprint7.Project.V2/FormMain.cs
@@ -0,0 +1,15 @@
+namespace Tyuiu.chernyhim.Sprint7.Project.V2
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
